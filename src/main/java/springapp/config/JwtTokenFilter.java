22843bad3f0adc00adbee022fbc14742fa4b7cd2
Yousif Seedhom <yousif.seedhom@gmail.com>
2019-04-10 05:41:22 -0500

    Add support for JSON Web Tokens

---
+++ src/main/java/springapp/config/JwtTokenFilter.java
@@ -0,0 +1,60 @@
+package springapp.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+import springapp.service.SecurityService;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    @Autowired
+    SecurityService securityService;
+
+    /**
+     * {@inheritDoc}
+     *
+     * Configures a filter that checks if the JWT passed in is valid.
+     * If so, updates the Security context, otherwise clears out the security context
+     *
+     */
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        String authHeader = request.getHeader("Authorization");
+        String token = null;
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            token = authHeader.substring(7);
+        }
+
+        Authentication authentication = null;
+        try{
+            authentication = securityService.isValid(token);
+        } catch (Exception e){
+            logger.info("Failed to get authentication using JWT Token: "+e.getMessage());
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        filterChain.doFilter(request, response);
+
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        if(new AntPathMatcher().match("/api/login", request.getServletPath())){
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+}
