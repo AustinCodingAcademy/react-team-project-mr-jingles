b084e6380e3d744205d4d1af5d2a8db37157e168
Hauser, Matthew <matthewhauser117@gmail.com>
2019-05-24 18:10:44 -0500

    edit header menu

--- react/src/containers/HeaderMenu.js
+++ react/src/containers/HeaderMenu.js
@@ -6,11 +6,11 @@ import logo from '../spot_on_light.png';
 class HeaderMenu extends Component {
   render= () => {
     let links = [
-      { label: 'Home', link: '#home', active: true, showforloggedinUser:true, ShowfornologgedinUser: true  },
-      { label: 'About', link: '#about', active: false , showforloggedinUser:true, ShowfornologgedinUser: true },
+      { label: 'Home', link: 'home', active: true, showforloggedinUser:true, ShowfornologgedinUser: true  },
+      { label: 'About', link: 'about', active: false , showforloggedinUser:true, ShowfornologgedinUser: true },
       { label: 'Clients', link: 'clients', active: false , showforloggedinUser:true, ShowfornologgedinUser: false },
       { label: 'Pets', link: 'pets', active: false , showforloggedinUser:true, ShowfornologgedinUser: false },
-      { label: 'Appointments', link: '#appointments' , active: false , showforloggedinUser:true, ShowfornologgedinUser: false },
+      { label: 'Appointments', link: 'appointments' , active: false , showforloggedinUser:true, ShowfornologgedinUser: false },
       { label: 'Login', link: 'login', active: false , showforloggedinUser:false, ShowfornologgedinUser: true },
       { label: 'Logout', link: 'logout', active: false , showforloggedinUser:true, ShowfornologgedinUser: false }
     ];
