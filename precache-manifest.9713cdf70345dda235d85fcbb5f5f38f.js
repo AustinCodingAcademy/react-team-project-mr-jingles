b8700b93e3a5f855691695dae04319f6eeaa97a5
Hauser, Matthew <matthewhauser117@gmail.com>
2019-05-24 17:54:06 -0500

    Updates

---
+++ precache-manifest.9713cdf70345dda235d85fcbb5f5f38f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bdbed7444ca233694ddb404385fca08",
+    "url": "/react-team-project-mr-jingles/static/media/spot_on_light.9bdbed74.png"
+  },
+  {
+    "revision": "18b4145d92f64c7f0e40",
+    "url": "/react-team-project-mr-jingles/static/js/runtime~main.1beb3bbb.js"
+  },
+  {
+    "revision": "bd1fa67f07962b3fb326",
+    "url": "/react-team-project-mr-jingles/static/js/main.64a54c63.chunk.js"
+  },
+  {
+    "revision": "fb347b34559b6a91518c",
+    "url": "/react-team-project-mr-jingles/static/js/2.1a50233e.chunk.js"
+  },
+  {
+    "revision": "bd1fa67f07962b3fb326",
+    "url": "/react-team-project-mr-jingles/static/css/main.07d2bab4.chunk.css"
+  },
+  {
+    "revision": "fb347b34559b6a91518c",
+    "url": "/react-team-project-mr-jingles/static/css/2.c29d13d7.chunk.css"
+  },
+  {
+    "revision": "ba4b632bb0ce2cd5f2ad5f4e04292dbf",
+    "url": "/react-team-project-mr-jingles/index.html"
+  }
+];
