d13fb4d688ce22c5ff0f4169dc2de97e4b226566
Yenugu <Indumathi_Yenugu@Dell.com>
2019-05-29 18:27:31 -0500

    added home page

---
+++ react/src/components/home.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import '../App.css';
+
+import logo from '../images/slide-1.jpg';
+
+
+class Home extends React.Component{     
+    
+
+
+    render = () => {
+      return (
+        <div>
+          
+                
+                   
+                    <div className='home'> <img src={logo} alt="hi" className='img_home'/></div>
+              
+      </div>
+      );
+    }
+  }
+  
+  export default Home;
+  
